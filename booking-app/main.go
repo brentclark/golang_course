8745b182c71ad880759ff52dbdac9946f663c70f
Brent <brent.clark@xneelo.com>
2024-11-13 15:34:04 +0200

    init

---
+++ booking-app/main.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main(){
+	var conferenceNAme = "Brents Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+
+	fmt.Println("Welcome to ", conferenceNAme , "out booking app ")
+	fmt.Println("We have", conferenceTickets, "and have", remainingTickets, "left")
+	fmt.Println("Get your ticket")
+}
+
+
